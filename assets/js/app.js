782f07d84ffd1b9af3d857d7266e6f00632b137e
lilas <lilasli@hotmail.fr>
2021-01-07 21:45:29 +0100

    Last release of the application

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,121 +1,198 @@
-//elments included inside $( document ).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
-$(document).ready(function () {
-const googleApiKey = "AIzaSyDVwtDeJjhuVK76P8sbZMnUHgSY7vjEC1A";
-let intitle = "";
-let inauthor = "inauthor:";
-const urlGoogleBook = "https://www.googleapis.com/books/v1/volumes?q=";
+/**
+ * Create a button to add a book in the pochlist
+ * @type {HTMLButtonElement}
+ */
 
-    //Get the div with content ID
-    const divContent = document.getElementById("content");
-    const myBookDiv = document.getElementById("myBooks");
-    const cancelButton = document.createElement("button");
-    const searchButton = document.createElement("button");
-    const addBookForm = document.createElement("FORM");
-    const bookSearchResults = document.createElement("div");
-
-    //Get the firstChild of the divContent
-    const firstChildContent = divContent.firstChild;
-
-//Create the new Button to add a book
-    const addBookButton = document.createElement("button");
-    addBookButton.innerHTML = "Ajouter un livre";
-
-//insert the new book button before the divContent
-    myBookDiv.insertBefore(addBookButton, divContent);
-
-//add ID to the new button
-    addBookButton.id = "addBookButton";
-    addBookButton.className = "btn btn-success btn-sm";
-
-//display a form when the addBookButton is clicked and hide the clicked button
-    addBookButton.addEventListener("click", function () {
-//add a form element to the document
-        // const addBookForm = document.createElement("FORM");
-        addBookForm.className = "text-left";
-        myBookDiv.insertBefore(addBookForm, divContent);
-//add the label to the title search input  to the form
-        const titleBarLabel = document.createElement("LABEL");
-        titleBarLabel.innerHTML = "Titre du livre";
-        addBookForm.appendChild(titleBarLabel);
-        titleBarLabel.className = "form-label";
-//add the title search input to the form
-        const titleBarSearch = document.createElement("INPUT");
-        titleBarSearch.setAttribute("type", "text");
-        titleBarSearch.ID = "titleSearchData";
-        addBookForm.appendChild(titleBarSearch);
-        titleBarSearch.className = "form-control";
-//add the label author search to the form
-        const authorBarLabel = document.createElement("LABEL");
-        authorBarLabel.innerHTML = "Auteur";
-        addBookForm.appendChild(authorBarLabel);
-        authorBarLabel.className = "form-label";
-//add the author search input to the form
-        const authorBarSearch = document.createElement("INPUT");
-        authorBarSearch.setAttribute("type", "text");
-        addBookForm.appendChild(authorBarSearch);
-        authorBarSearch.className = "form-control";
-//add the search button to the form
-        //const searchButton = document.createElement("button");
-        searchButton.innerHTML = "Rechercher";
-        searchButton.ID = "search-button";
-        addBookForm.appendChild(searchButton);
-        searchButton.className = "btn btn-secondary btn-success btn-sm btn-block";
-//add the cancel button to the form
-        //cancelButton = document.createElement("button");
-        cancelButton.innerHTML = "Annuler";
-        addBookForm.appendChild(cancelButton);
-        cancelButton.className = "btn btn-annuler btn-sm btn-block";
+//Création d'un bouton pour ajouter un livre au chargement de la page
+const addBookButton = document.createElement("button");
+addBookButton.innerHTML = "Ajouter un livre";
+document
+  .getElementById("myBooks")
+  .insertBefore(addBookButton, document.getElementById("content"));
+addBookButton.id = "addBookButton";
+addBookButton.className = "btn btn-success btn-sm";
 
 
+/**
+ * Create a form in html to find book from google books API;
+ * @type {string}
+ */
+const formDiv = `<div class="text-left" id="searchBookForm">
+        <form id="bookForm" class="text-left">
+        <label class="form-label">Titre du livre</label>
+        <input id= "bookTitle" type="text" class="form-control">
+            <label class="form-label">Auteur</label>
+            <input id="author" type="text" class="form-control">
+            <label for="nbAfficher" class="form-label">Nombre de résultats souhtaités:</label>
+            <select name="nbAfficher" id="nbAfficher" class="form-control col-lg-3 col-md-5">
+            <option value="10">--Merci de choisir une option--</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+            </select>
+            </form>
+            <button id="search-button" class="btn btn-secondary btn-success btn-sm btn-block">Rechercher</button>
+            <button id="cancel-button" class="btn btn-annuler btn-sm btn-block">Annuler</button>
+                     
+        </div>`;
 
-        bookSearchResults.id = "bookSearchResults";
+/**
+ * Create a div to store the book result search
+ * @type {string}
+ */
+const searchBookResults = `<div id="searchBookResults">
+            <h3 class="text-center h3">Resultats de recherche</h3>
+            <div id="booksList" class="cardsGroup row"></div>
+            </div>`;
 
-        myBookDiv.insertBefore(bookSearchResults, divContent);
-        addBookButton.hidden = true;
-        //displaySearchResults();
+//Formulaire de recherche d'un livre
+const addBookForm = document.createElement("div");
+addBookForm.id = "addBookForm";
+document
+  .getElementById("myBooks")
+  .insertBefore(addBookForm, document.getElementById("content"));
+addBookForm.innerHTML += formDiv;
+addBookForm.style.visibility = "hidden";
+addBookForm.style.display = "none";
 
-    }, {once: true});// the clicked button should work once
+//Disparition du bouton et apparition du formulaire au click du bouton
+$("#addBookButton").click(function () {
+  addBookForm.style.visibility = "visible";
+  addBookForm.style.display = "block";
+  this.style.visibility = "hidden";
+  this.style.display = "none";
+  document.getElementById("searchBookResults").style.visibility = "hidden";
+  document.getElementById("searchBookResults").style.display = "none";
+});
+$("#searchBookForm").append(searchBookResults);
 
+$("#cancel-button").click(function (){
+  addBookForm.style.visibility = "hidden";
+  addBookForm.style.display = "none";
+  $("#addBookButton").css("visibility","visible");
+  $("#addBookButton").css("display","block");
 
-    function displaySearchResults() {
-
-        const headerSearch = document.createElement("h1");
-        headerSearch.innerHTML ="Resultats de recherche";
-
-        const placeHolder = document.createElement("img");
-        placeHolder.src = "https://via.placeholder.com/150";
-
-        myBookDiv.insertBefore(bookSearchResults, divContent);
-        bookSearchResults.appendChild(headerSearch);
-        bookSearchResults.appendChild(placeHolder);
-
-    }
-
-    $("#searchButton").click(function (e) {
-        e.preventDefault();
-        displaySearchResults();
-           searchButton.hidden = false;
-    },{once:false});
-
-    function formattingSearchResults() {
-        const cards = `<div class="card" style="width: 18rem;">
-  <img src="https://via.placeholder.com/150\n" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>`
-return cards;
-
-    }
-    return false;
 })
+/**
+ * Formatting an object to html through Bootstrap card
+ * @param obj
+ * @returns {HTMLDivElement}
+ */
+const formattingSearchResults = (obj) => {
+  const cards = document.createElement("div");
+  //document.getElementById("searchBookResults").appendChild(cards);
+  cards.className = "card col-lg-6 col-md-5 justify-content-center";
+  cards.id = obj.identifier;
+
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body m-auto";
+  cards.appendChild(cardBody);
+
+  const title = document.createElement("h5");
+  title.className = "card-title";
+  title.innerHTML = `Titre : ${obj.title}`;
+  cardBody.appendChild(title);
+
+  const identifier = document.createElement("h5");
+  identifier.className = "livre-identifier";
+  identifier.innerHTML = `Id : ${obj.identifier}`;
+  cardBody.appendChild(identifier);
+
+  const author = document.createElement("p");
+  author.className = "";
+  author.innerHTML = `Auteur(e) : ${obj.author}`;
+  cardBody.appendChild(author);
+
+  const description = document.createElement("p");
+  description.className = "card-text text-justify";
+  description.innerHTML = `Description : ${obj.description}`;
+  cardBody.appendChild(description);
+
+  const cardImage = document.createElement("img");
+  cardImage.src = obj.bookImage;
+  cardImage.className = "card-img-bottom";
+  cardImage.alt = obj.title;
+  cards.appendChild(cardImage);
+
+  const icon = document.createElement("button");
+  cardBody.appendChild(icon);
+  icon.className = "button";
+  icon.innerHTML = "<i class='fas fa-bookmark'></i>";
+
+  //click function to saved book in the pochlist
+  icon.onclick = function () {
+    saveBook(obj);
+    console.log(obj.identifier);
+    displaySavedBook();
+  };
+  return cards;
+};
+
+
+/**
+ * Formatting an object to html through Bootstrap card for the saved book
+ * @param obj
+ * @returns {HTMLDivElement}
+ */
+const formattingSavedResults = (obj) => {
+  const cardsSaved = document.createElement("div");
+  //document.getElementById("searchBookResults").appendChild(cards);
+  cardsSaved.className = "card col-lg-6 col-md-5 justify-content-center";
+  cardsSaved.id = obj.identifier;
+
+  const cardBodySaved = document.createElement("div");
+  cardBodySaved.className = "card-body m-auto";
+  cardsSaved.appendChild(cardBodySaved);
+
+  const titleBookSaved = document.createElement("h5");
+  titleBookSaved.className = "card-title";
+  titleBookSaved.innerHTML = `Titre : ${obj.title}`;
+  cardBodySaved.appendChild(titleBookSaved);
+
+  const identifierSaved = document.createElement("h5");
+  identifierSaved.className = "livre-identifier";
+  identifierSaved.innerHTML = `Id : ${obj.identifier}`;
+  cardBodySaved.appendChild(identifierSaved);
+
+  const authorSaved = document.createElement("p");
+  authorSaved.className = "";
+  authorSaved.innerHTML = `Auteur(e) : ${obj.author}`;
+  cardBodySaved.appendChild(authorSaved);
+
+  const descriptionSaved = document.createElement("p");
+  descriptionSaved.className = "card-text text-justify";
+  descriptionSaved.innerHTML = `Description : ${obj.description}`;
+  cardBodySaved.appendChild(descriptionSaved);
+
+  const cardImageSaved = document.createElement("img");
+  cardImageSaved.src = obj.bookImage;
+  cardImageSaved.className = "card-img-bottom";
+  cardImageSaved.alt = obj.title;
+  cardsSaved.appendChild(cardImageSaved);
+
+  const iconTrash = document.createElement("button");
+  cardBodySaved.appendChild(iconTrash);
+  iconTrash.className = "button";
+  iconTrash.innerHTML = "<i class='fas fa-trash-alt'></i>";
+
+
+  /**
+   * onclick function to remove book from the pochList
+   */
+  iconTrash.onclick = function () {
+    console.log(obj.identifier);
+    removedBook(obj.identifier);
+    displaySavedBook();
+  };
+  return cardsSaved;
+};
+
+/**
+ * Create a div where all books saved are displayed
+ * @type {HTMLDivElement}
+ */
+const bookList = document.createElement("div");
+document.getElementById("content").appendChild(bookList);
+bookList.id = "bookList";
+bookList.className = "row";
+
